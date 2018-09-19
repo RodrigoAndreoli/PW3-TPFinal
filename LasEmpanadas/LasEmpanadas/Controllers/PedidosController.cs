bd8dd3475638476983b121e8cfb92fb4f618eb25
RodrigoAndreoli <rodrigoandreoli09@gmail.com>
2018-09-19 19:38:29 -0300

    Commit 2 - Estructura completa

---
+++ LasEmpanadas/LasEmpanadas/Controllers/PedidosController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LasEmpanadas.Controllers
+{
+    public class PedidosController : Controller
+    {
+
+        public ActionResult Iniciar()
+        {
+            return View();
+        }
+
+        public ActionResult Iniciado()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+        public ActionResult Elegir()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+
+    }
+}
