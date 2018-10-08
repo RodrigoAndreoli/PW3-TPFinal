b7344deb4ec92493282d233903ed6f948c3080c2
g-rosinski <gonzarosinski@gmail.com>
2018-10-08 01:21:58 -0300

    Agrego los models base

---
+++ LasEmpanadas/LasEmpanadas/Models/PedidoUsuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LasEmpanadas.Models
+{
+    public class PedidoUsuario
+    {
+        public int id { get; set; }
+        public Usuario usuario { get; set; }
+        public List<Tuple<int, string>> cantidadYGusto { get; set; } // Cantidad, Gusto
+        public enum seleccionLista { SI, NO }
+    }   
+}
---
+++ LasEmpanadas/LasEmpanadas/Models/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LasEmpanadas.Models
+{
+    public class Usuario
+    {
+        private int id { get; set; }
+        private string mail { get; set; }
+        private string usuario { get; set; }
+        private string contrasenia { get; set; }
+    }
+}
