05c97507324bba6a63cbd2117e901a170f77da9a
g-rosinski <gonzarosinski@gmail.com>
2018-10-17 16:30:04 -0300

    Cambio los atributos a publico

--- LasEmpanadas/LasEmpanadas/Models/Pedido.cs
+++ LasEmpanadas/LasEmpanadas/Models/Pedido.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace LasEmpanadas.Models
 {
     public class Pedido
     {
         private int id { get; set; }
-        private string token { get; set; }
-        private String nombre { get; set; }
-        private int precioUnidad { get; set; }
-        private int precioDocena { get; set; }
-        private List<string> gustosDisponibles { get; set; }
-        private Usuario responsable { get; set; }
-        private List<PedidoUsuario> invitados { get; set; }
-        private enum estado {
+        public string token { get; set; }
+        [Display(Name ="Nombre del local")]
+        public String nombre { get; set; }
+        [Display(Name = "Precio por unidad")]
+        public int precioUnidad { get; set; }
+        [Display(Name = "Precio por docena")]
+        public int precioDocena { get; set; }
+        [Display(Name = "Gustos disponibles")]
+        public List<string> gustosDisponibles { get; set; }
+        public Usuario responsable { get; set; }
+        [Display(Name = "Invitados")]
+        public List<PedidoUsuario> invitados { get; set; }
+        public enum estado {
             Abierto,
             Pendiente,
             Cerrado
