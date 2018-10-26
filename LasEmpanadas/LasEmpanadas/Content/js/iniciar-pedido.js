dbfad3084de691e1ebc629e2858f927c9403c47e
nicolaslucer0 <nicolas.lucero1@gmail.com>
2018-10-25 22:54:12 -0300

    Diseño de iniciar pedido

---
+++ LasEmpanadas/LasEmpanadas/Content/js/iniciar-pedido.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function () {
+    $('.multiple').select2();
+
+    /** MULTIPLE EMAIL INPUT */
+    $(".enter-mail-id").keydown(function (e) {
+        if (e.keyCode == 13 || e.keyCode == 32) {
+            //alert('You Press enter');
+            var getValue = $(this).val();
+            $('.all-mail').append('<span class="email-ids">' + getValue + ' <span class="cancel-email">x</span></span>');
+            $(this).val('');
+        }
+    });
+
+    /// Cancel
+    $(document).on('click', '.cancel-email', function () {
+        $(this).parent().remove();
+    });
+    // $('.enter-mail-id').click()
+
+});
