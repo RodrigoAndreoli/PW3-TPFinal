1462198dfb80b9eb7a7b3aa6f457931490588719
damian-villarreal <damianvr@gmail.com>
2018-11-16 00:19:15 -0300

    update de commit anterior

--- LasEmpanadas/LasEmpanadas/Models/Partials/Pedido.cs
+++ LasEmpanadas/LasEmpanadas/Models/Partials/Pedido.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace LasEmpanadas.Models
 {
+    [MetadataType(typeof(PedidoMetadata))]
     public partial class Pedido
     {
         public int[] GustoEmpanadaDisponibles { get; set; }
