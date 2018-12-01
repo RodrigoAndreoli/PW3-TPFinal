c3ac7669fd0d6f45aa2390ef8a185b49bbaa61d4
g-rosinski <gonzarosinski@gmail.com>
2018-12-01 06:14:31 -0300

    Modifico front de elegir gustos

---
+++ LasEmpanadas/LasEmpanadas/Content/js/elegir-pedido.js
@@ -0,0 +1,24 @@
+﻿$(document).ready(function () {
+    calcularSubtotal();
+    calcularTotal();
+    $(".cantidad").change(calcularSubtotal);
+    $(".cantidad").change(calcularTotal);
+});
+
+function calcularSubtotal() {
+    var unidad = parseInt($("#precio-unidad").html());
+    var docena = parseInt($("#precio-docena").html());
+    var subtotales = $(".precio-subtotal");
+    $(".cantidad").each(function (i) {
+        subtotalDocena = Math.floor($(this).val() / 12) * docena; // Calculo cantidad de docenas y las multiplico por el precio de la docena
+        subtotalUnidad = ($(this).val() % 12) * unidad; // Calculo cantidad de unidades restantes a docenas y las multiplico por el precio de la unidad
+        subtotales[i].innerText = subtotalDocena + subtotalUnidad;
+    });
+}
+function calcularTotal() {
+    var total = 0;
+    $(".precio-subtotal").each(function () {
+        total += parseInt( $(this).html() );
+    });
+    $("#precio-total").html(total);
+}
